874926bf15e23839de2083008c3c0bcb50fce75e
Evandro oliveira <vandoaparecido@hotmail.com>
2020-06-21 15:00:24 -0300

    Resolvendo bugs e validando campos

--- scripts/create-point.js
+++ scripts/create-point.js
@@ -1,8 +1,9 @@
 
 
 function populateUFs(){
+
     const ufSelect = document.querySelector("select[name=uf]")
- 
+     
     fetch("https://servicodados.ibge.gov.br/api/v1/localidades/estados")
   //.then((res)=>{return res,json()})
     .then( res => res.json() )
@@ -23,9 +24,11 @@ populateUFs()
 
 
 function getCities(event) {
+  //const citySelect = document.querySelector("select [name=city]")
     const citySelect = document.querySelector("[name=city]")
     const stateInput = document.querySelector("[name=state]")
-
+  
+    //console.log(event.target.value)
     const ufValue = event.target.value
 
     const indexOfSelectedState = event.target.selectedIndex
@@ -34,11 +37,19 @@ function getCities(event) {
 
     const url = `https://servicodados.ibge.gov.br/api/v1/localidades/estados/${ufValue}/municipios`
 
+    //Limpa cidades
+    citySelect.innerHTML = "<option value>selecione a cidade</option>"
+    //desabilita o campo
+    citySelect.disabled = true
+
     fetch(url)
     .then( res => res.json() )
     .then( cities => {
+        
         for( const city of cities ) {
-            citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+             //ufSelect.innerHTML = ufSelect.innerHTML +'<option value="1">Valor</option>' 
+            //citySelect.innerHTML += `<option value="${city.id}">${city.nome}</option>`
+            citySelect.innerHTML += `<option value="${city.name}">${city.nome}</option>`
         }
 
         citySelect.disabled = false
@@ -49,3 +60,6 @@ function getCities(event) {
 document
     .querySelector("select[name=uf]")
     .addEventListener("change", getCities) 
+    //.addEventListener("change", () => {
+//    console.log("Mudei")
+//})
