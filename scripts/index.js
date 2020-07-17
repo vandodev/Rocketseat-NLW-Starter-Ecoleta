befa5f75190c3f83c0c65e229b028dfe1ac947a1
Evandro oliveira <vandoaparecido@hotmail.com>
2020-07-17 01:41:25 -0300

    Correção janela modal, navegação e imagens do botão

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,10 @@
-const buttonSearch = document.querySelector ("#page-home main a")
+const buttonSearch = document.querySelector("#page-home main a")
 const modal = document.querySelector("#modal")
 const close = document.querySelector("#modal .header a")
 
+
 buttonSearch.addEventListener("click", ()=>{
-    modal.classList.remove("hide")
+    modal.classList.remove("hide")    
 })
 
 close.addEventListener("click", ()=>{
