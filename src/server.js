f4945991000559aa44a4b3407053b4c1df931cb2
Evandro oliveira <vandoaparecido@hotmail.com>
2020-07-19 01:11:32 -0300

    Criação de rotas

--- src/server.js
+++ src/server.js
@@ -1,5 +1,21 @@
 const express = require("express")
 const server = express()
 
+server.use(express.static("public"))
+
 //Ligar o servidor
 server.listen(3000)
+
+//Crindo rotas
+server.get("/", (req, res) => {
+    res.sendFile(__dirname + "/views/index.html")
+})
+
+server.get("/create-point", (req, res) => {
+    res.sendFile(__dirname + "/views/create-point.html")
+})
+
+server.get("/search-results", (req, res) => {
+    res.sendFile(__dirname + "/views/search-results.html")
+})
+search-results
